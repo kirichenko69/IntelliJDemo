65417b95d2869af8f40a83fbf36411d871be23db
@kirichenko69 <jackson5@mail.ru>
2018-04-06 19:35:20 +0300

    Много правок

--- src/main/java/local/home/jackson5/java/BitIndex.java
+++ src/main/java/local/home/jackson5/java/BitIndex.java
@@ -1,28 +1,42 @@
 package local.home.jackson5.java;
 
 public class BitIndex {
-    private long data = 0;
+    private long data;
+
+    BitIndex(boolean allTrue){
+        data = allTrue ? -1 : 0;
+        //if (allTrue) {
+        //    data = -1;
+        //} else {
+        //    data = 0;
+        //}
+    }
+
     public boolean get(int index) {
-        //todo
-        throw new UnsupportedOperationException();
+        return (data & ( 1 << index )) != 0;
     }
     public void set(int index, boolean value) {
-        if (value) {
-            data = data | ( 1 << index ); // 00000001
-        } else {
-            data = data & ( 1 << index ); // 00000001 потом инвертируем
-        }
+        data = value ? data | ( 1 << index ) : data & ~( 1 << index );
+        //if (value) {
+        //    data = data | ( 1 << index ); // 00000001
+        //} else {
+        //    data = data & ~( 1 << index ); // 00000001 потом инвертируем
+        //}
     }
 }
 
 class BitIndexTest {
     public static void main(String[] args) {
-        BitIndex index = new BitIndex();
+        BitIndex index = new BitIndex( false );
         index.set(19, true);
         index.set(62, true);
         index.get(29);
         System.out.println(index.get(29));
         System.out.println(index.get(19));
+        System.out.println(index.get(62));
+        System.out.println(index.get(0));
+        System.out.println(index.get(1));
+
 
     }
 }
